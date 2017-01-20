c4095be02389a03ccd3cc10b867aefc5b13bac33
Lydia Chang <mathmerized@gmail.com>
2017-01-20 12:39:00 -0500

    initial go at UI/controller

--- app/controllers/ntd_forms/steps_controller.rb
+++ app/controllers/ntd_forms/steps_controller.rb
@@ -18,11 +18,31 @@ class NtdForms::StepsController < FormAwareController
   def show
 
     add_breadcrumb @form_type.name.pluralize(2), form_path(@form_type)
-    add_breadcrumb "New"
-    #add_breadcrumb step
+    add_breadcrumb step.to_s.titleize
 
-    @has_next_step = next_step?(step)
-    @has_prev_step = previous_step?(step)
+    # get data for the current form
+    # builds the appropritae association / sub-form
+    # TODO: use service to query assets and their associated tables
+    # where NtdService.get_data runs its queries and returns an array of hashes
+    case step
+      when :agency_information
+        # do nothing
+      when :admin_and_maint_facility_inventory
+        # @form.ntd_admin_and_maintenance_facilities.build(NtdReportingService.get_data(step))
+      when :passenger_and_parking_facility_inventory
+        # @form.ntd_passenger_and_parking_facilities.build(NtdReportingService.get_data(step))
+      when :service_vehicle_inventory
+        # @form.ntd_service_vehicle_fleets.build(NtdReportingService.get_data(step))
+      when :revenue_vehicle_inventory
+        # @form.ntd_revenue_vehicle_fleets.build(NtdReportingService.get_data(step))
+      when :summary
+        # if no other sub reports
+        # generate those first
+        # else
+        # generate A-80
+    end
+
+    @has_prev_step = previous_step
 
     render_wizard
 
@@ -33,7 +53,25 @@ class NtdForms::StepsController < FormAwareController
     add_breadcrumb @form_type.name.pluralize(2), form_path(@form_type)
     add_breadcrumb "New"
 
-    @form.update_attributes(form_params)
+
+    case step
+      when :agency_information
+        @form.update_attributes(form_params)
+      when :admin_and_maint_facility_inventory
+        # @form.ntd_admin_and_maintenance_facilities.create(NtdReportingService.get_data(step))
+      when :passenger_and_parking_facility_inventory
+        # @form.ntd_passenger_and_parking_facilities.create(NtdReportingService.get_data(step))
+      when :service_vehicle_inventory
+        # @form.ntd_service_vehicle_fleets.create(NtdReportingService.get_data(step))
+      when :revenue_vehicle_inventory
+        # @form.ntd_revenue_vehicle_fleets.create(NtdReportingService.get_data(step))
+      when :summary
+        # if no other sub reports
+        # generate those first
+        # else
+        # generate A-80
+    end
+
     render_wizard @form
   end
 
