f620832a5237839a9620b28eff2c0d86dfa2bd45
mmaranda-cs <mmaranda@camsys.com>
2017-01-24 10:54:09 -0500

    NTD Revenue vehicle returning data. Next step integrating it with the controllers.

--- app/services/ntd_reporting_service.rb
+++ app/services/ntd_reporting_service.rb
@@ -12,7 +12,7 @@ class NtdReportingService
 
   #------------------------------------------------------------------------------
   #
-  # Instantcs Methods
+  # Instance Methods
   #
   #------------------------------------------------------------------------------
 
@@ -21,58 +21,88 @@ class NtdReportingService
   # the columns which need it
   def revenue_vehicle_fleets(org)
 
-    # We have to use a native SQL rather than going through the model as
-    # complete models are not returned and the initalizers cause method not found
-    # exceptions.
-    results = ActiveRecord::Base.connection.execute(%q{
-      SELECT
-      count(*) AS 'size',
-      a.fta_vehicle_type_id,
-      a.fta_funding_type_id,
+    # # We have to use a native SQL rather than going through the model as
+    # # complete models are not returned and the initalizers cause method not found
+    # # exceptions.
+    asset_type_id = AssetType.where(name: 'Revenue Vehicles').pluck(:id)
+    organizations = [org]
+    # organizations = org.id
 
-      a.manufacture_year,
-      a.rebuild_year,
-      a.manufacturer_id,
-      a.manufacturer_model,
-      fuel_type_id,
-      a.vehicle_length,
-      a.seating_capacity,
-      a.standing_capacity
+
+    sql = "SELECT
+        null AS rvi_id,
+        count(*) AS 'size',
+        null AS 'num_active',
+        null AS 'num_ada_accessible',
+        null AS 'num_emergency_contingency',
+        a.fta_vehicle_type_id AS vehicle_type,
+        a.fta_funding_type_id AS funding_source,
+        manufacturer_id AS manufacture_code,
+        a.manufacturer_model AS model_number,
+        a.manufacture_year AS manufacture_year,
+        a.rebuild_year AS renewal_year,
+        null AS renewal_type,
+        null AS renewal_cost,
+        null AS renewal_cost_year,
+        null AS replacement_cost,
+        null AS replacement_cost_parts,
+        null AS replacement_cost_warranty,
+        a.fuel_type_id AS fuel_type,
+        a.vehicle_length AS vehicle_length,
+        a.seating_capacity AS seating_capacity,
+        a.standing_capacity AS standing_capacity,
+        null AS total_active_miles_in_period,
+        null AS avg_lifetime_active_miles,
+        null AS notes
       FROM
-      assets a
+        assets a
       WHERE
-      a.asset_type_id = 1
-      AND a.organization_id = 6
+        a.asset_type_id IN (#{asset_type_id.join(',')})
+      AND
+        a.organization_id IN (#{organizations.join(',')})
       GROUP BY
-      a.fta_vehicle_type_id,
-      a.fta_funding_type_id,
-      a.manufacture_year,
-      a.rebuild_year,
-      a.manufacturer_id,
-      a.manufacturer_model,
-      a.fuel_type_id,
-      a.vehicle_length,
-      a.seating_capacity,
-      a.standing_capacity
-      })
+        vehicle_type,
+        funding_source,
+        manufacture_year,
+        renewal_year,
+        manufacture_code,
+        model_number,
+        fuel_type,
+        vehicle_length,
+        seating_capacity,
+        standing_capacity"
+
+    results = ActiveRecord::Base.connection.execute(sql)
 
     # Convert the results set to an array of hashes
     fleets = []
     results.each do |row|
       fleet = {
-        :organization_id => org.id,
-        :asset_type_id => 1,
-        :size => row[0],
-        :fta_vehicle_type_id => row[1],
-        :fta_funding_type_id => row[2],
-        :manufacture_year => row[3],
-        :rebuild_year => row[4],
-        :manufacturer_id => row[5],
-        :manufacturer_model => row[6],
-        :fuel_type_id => row[7],
-        :vehicle_length => row[8],
-        :seating_capacity => row[9],
-        :standing_capacity => row[10]
+        :size => row[1],
+        :organization_id => org,
+        :asset_type_id => asset_type_id,
+        :num_active => row[2],
+        :num_ada_accessible => row[3],
+        :num_emergency_contingency => row[4],
+        :fta_vehicle_type_id => row[5],
+        :funding_source => row[6],
+        :manufacture_code => row[7],
+        :model_number => row[8],
+        :manufacture_year => row[9],
+        :renewal_year => row[10],
+        :renewal_cost => row[11],
+        :renewal_type => row[12],
+        :renewal_cost_year => row[13],
+        :replacement_cost => row[14],
+        :replacement_cost_parts => row[15],
+        :replacement_cost_warranty => row[16],
+        :fuel_type => row[17],
+        :vehicle_length => row[18],
+        :seating_capacity => row[19],
+        :standing_capacity => row[20],
+        :total_active_miles_in_period => row[21],
+        :avg_lifetime_active_miles => row[22],
+        :notes => row[23]
       }
       # calculate the additional properties and merge them into the results
       # hash
@@ -92,14 +122,20 @@ class NtdReportingService
   # needed for the report.
   def calc_fleet_items(fleet_group)
 
-    vehicles = Vehicle.where(fleet_group.except(:size))
-    total_active_miles_in_period = 0
-    avg_lifetime_active_miles = 0
+    vehicles = Vehicle.where(organization_id: fleet_group[:organization_id], asset_type_id: fleet_group[:asset_type_id], fta_vehicle_type_id: fleet_group[:fta_vehicle_type_id],
+                             fta_funding_type_id: fleet_group[:funding_source], manufacturer_id: fleet_group[:manufacture_code], manufacturer_model: fleet_group[:model_number],
+                             manufacture_year: fleet_group[:manufacture_year], rebuild_year: fleet_group[:renewal_year], fuel_type_id: fleet_group[:fuel_type],
+                             vehicle_length: fleet_group[:vehicle_length], seating_capacity: fleet_group[:seating_capacity], standing_capacity: fleet_group[:standing_capacity])
+
     num_active = 0
     num_ada_accessible = 0
     num_emergency_contingency = 0
+    total_active_miles_in_period = 0
+    avg_lifetime_active_miles = 0
     replacement_cost = 0
     replacement_cost_year = current_fiscal_year_year
+
+    puts(vehicles)
 
     vehicles.each do |vehicle|
 
