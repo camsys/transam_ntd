06026214b62da3aaa52c1182ed6b47e1799ea114
mathmerized <mathmerized@gmail.com>
2018-03-27 12:07:34 -0400

    fix syntax in data migration

--- db/data_migrations/20180327144518_change_support_vehicle_asset_fleet_type.rb
+++ db/data_migrations/20180327144518_change_support_vehicle_asset_fleet_type.rb
@@ -1,5 +1,5 @@
 class ChangeSupportVehicleAssetFleetType < ActiveRecord::DataMigration
   def up
-    AssetFleetType.find_by(class_name: 'SupportVehicle').update!("asset_type_id,asset_subtype_id,fta_support_vehicle_type_id,manufacture_year,pcnt_capital_responsibility")
+    AssetFleetType.find_by(class_name: 'SupportVehicle').update!(groups: "asset_type_id,asset_subtype_id,fta_support_vehicle_type_id,manufacture_year,pcnt_capital_responsibility")
   end
 end
