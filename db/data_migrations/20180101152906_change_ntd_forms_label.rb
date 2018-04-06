391cb55d829f50520f29e50c10d1d44205a2d4d1
mathmerized <mathmerized@gmail.com>
2018-04-06 11:40:36 -0400

    TTPLAT-124 cleanup data migration for ntd reporting form as form might not exist

--- db/data_migrations/20180101152906_change_ntd_forms_label.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNtdFormsLabel < ActiveRecord::DataMigration
-  def up
-    Form.find_by(controller: 'ntd_forms').update!(name: 'NTD Reporting') if Form.find_by(controller: 'ntd_forms').present?
-  end
-end
